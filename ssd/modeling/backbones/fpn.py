ceadba3961fa4eba2ff839f9c83700fd70a0273f
maxschau <max.torre.schau@gmail.com>
2022-04-07 11:51:38 +0200

    plz help

--- ssd/modeling/backbones/fpn.py
+++ ssd/modeling/backbones/fpn.py
@@ -4,6 +4,10 @@ import torch
 from torchvision.models.feature_extraction import get_graph_node_names, create_feature_extractor
 from torchsummary import summary
 from typing import Tuple, List
+from collections import OrderedDict
+
+LOWEST_BACKBONE_LVL = 2   # E.g., "conv2"-like level
+HIGHEST_BACKBONE_LVL = 5  # E.g., "conv5"-like level
 
 class FPNModel(nn.Module):
     def __init__(
@@ -15,70 +19,34 @@ class FPNModel(nn.Module):
         super().__init__()
         
         self.out_channels = output_channels
-        self.output_feature_shape = output_feature_sizes
+        self.output_feature_shape = output_feature_sizes 
         self.image_channels = image_channels
-        resnet = torchvision.models.resnet34(pretrained=True)
-        self.body = create_feature_extractor(resnet, 
-                                             return_nodes={
-                                                 f'layer{k}': str(v) for v, k in enumerate([1, 2, 3, 4])})
+        self.model = torchvision.models.resnet34(pretrained=True).to('cuda')
+        self.layers = {'layer1': 'layer1','layer2': 'layer2','layer3': 'layer3','layer4': 'layer4',}
+        self.body = create_feature_extractor(
+            self.model, return_nodes=self.layers)
         
-        self.extra = nn.ModuleList([
-            torch.nn.Sequential(
-                torch.nn.Conv2d(
-                    in_channels=512,
-                    out_channels=512,
-                    kernel_size=3,
-                    stride=1,
-                    padding=1,
-                ),
-                torch.nn.ReLU(),
-                torch.nn.Conv2d(
-                    in_channels=512,
-                    out_channels=256,
-                    kernel_size=3,
-                    stride=2,
-                    padding=1,
-                ),
-                torch.nn.ReLU(),
-            ),
-            torch.nn.Sequential(
-                torch.nn.Conv2d(
-                    in_channels=256,
-                    out_channels=256,
-                    kernel_size=2,
-                    stride=1,
-                    padding=1,
-                ),
-                torch.nn.ReLU(),
-                torch.nn.Conv2d(
-                    in_channels=256,
-                    out_channels=256,
-                    kernel_size=2,
-                    stride=2,
-                    padding=0,
-                ),
-                torch.nn.ReLU(),
-            )
-        ])
-
-        
-    def init_from_pretrain(self, state_dict):
-        self.resnet.load_state_dict(state_dict)
     
     def forward(self, x):
-        a = self.body(x)
-        out_features=list(a.values())
-        x = out_features[-1]
-#         print("START EXTRA")
-        for layer in self.extra:
-            x = layer(x)
-            out_features.append(x)
-#         print("END EXTRA")
-#         print("START OUTFEATURES")
-#         for f in out_features:
-#             print(f.shape)
-#         print("END OUTFEATURES")
-            
+        out_features = self.body(x).values()
+        
+        
+        
+        
+        
+        
+        
+        '''
+        x = OrderedDict()
+        for idx, feat in enumerate(out_features):
+            x[f"feat{idx}"] = feat
+        m = torchvision.ops.FeaturePyramidNetwork(in_channels,256)
+        m = m.to("cuda")
+        
+        output = m(x)
+        
+        out_features = list(output.values())
+        '''
         for idx, feature in enumerate(out_features):
             out_channel = self.out_channels[idx]
             h, w = self.output_feature_shape[idx]
@@ -89,5 +57,5 @@ class FPNModel(nn.Module):
         assert len(out_features) == len(
             self.output_feature_shape
         ), f"Expected that the length of the outputted features to be: {len(self.output_feature_shape)}, but it was: {len(out_features)}"
-        return tuple(out_features)
+        return tuple(list(output.values()))
 
