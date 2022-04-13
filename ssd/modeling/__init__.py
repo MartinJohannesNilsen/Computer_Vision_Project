57c7f74c23204a50edbb0825be2571967ffbaff3
Zaim Ul-Abrar Imran <zuimran@oppdal.idi.ntnu.no>
2022-04-13 13:03:59 +0200

    focal loss init

--- ssd/modeling/__init__.py
+++ ssd/modeling/__init__.py
@@ -2,3 +2,4 @@ from .anchor_boxes import AnchorBoxes
 from .anchor_encoder import AnchorEncoder
 from .ssd_multibox_loss import SSDMultiboxLoss
 from .ssd import SSD300
+from .focal_loss import FocalLoss
